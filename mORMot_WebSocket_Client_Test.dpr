91e208d244caf3a5f77330419d586173b9cfa105
PrometeusRec <98043953+PrometeusRec@users.noreply.github.com>
2022-03-13 00:11:21 -0500

    Add files via upload

---
+++ mORMot_WebSocket_Client_Test.dpr
@@ -0,0 +1,73 @@
+program Project31WinHTTPEchoServer;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.Classes, mormot.net.client, mormot.core.json, mormot.core.datetime, mormot.net.ws.client, mormot.net.http, mormot.net.ws.core, mormot.core.interfaces, mormot.core.unicode,
+  mormot.net.ws.server, mormot.net.sock, mormot.core.base;
+
+type
+     TWSSRequests = class
+       public
+         procedure DealWSSRequests(Sender : TWebCrtSocketProcess; const Frame: TWebSocketFrame);
+     end;
+
+var WSSResponses  : TStringList;
+    endConnection : Boolean;
+
+procedure TWSSRequests.DealWSSRequests(Sender : TWebCrtSocketProcess; const Frame: TWebSocketFrame);
+
+begin
+  case Frame.opcode of
+    focContinuation    : ;
+    focText            : WSSResponses.Add(Frame.payload);
+    focBinary          : ;
+    focConnectionClose : endConnection := true;
+    focPing            : ;
+    focPong            : ;
+  end;
+end;
+
+
+procedure DoWSS;
+
+var ClientWS      : THttpClientWebSockets;
+    WSSConnection : TWSSRequests;
+    lProto        : TWebSocketProtocolChat;
+    msg           : RawUtf8;
+
+begin
+  WSSConnection := TWSSRequests.Create;
+  lProto := TWebSocketProtocolChat.Create('TestWSS','',WSSConnection.DealWSSRequests);
+  ClientWS := THttpClientWebSockets.Create;
+  ClientWS.OnCallbackRequestProcess := nil;
+  while (not endConnection) do
+    ;
+
+//  ClientWS.Open('ws.postman-echo.com', '443', nlTcp, 10000, true);
+//  msg := ClientWS.WebSocketsUpgrade('raw', '', false, [], lProto, ''); // {   "op": "ping" }
+
+//  ClientWS.Open('demo.piesocket.com', '443', nlTcp, 10000, true);
+//  msg := ClientWS.WebSocketsUpgrade('v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self', '', false, [], lProto, '');
+
+//  ClientWS.Open('stream.data.sandbox.alpaca.markets', '443', nlTcp, 10000, true);
+//  msg := ClientWS.WebSocketsUpgrade('v2/iex', '', false, [], lProto, '');
+
+//  ClientWS.Open('ws.blockchain.info', '443', nlTcp, 10000, true); // {   "op": "ping" }
+//  msg := ClientWS.WebSocketsUpgrade('inv', '', false, [], lProto, '');
+
+  ClientWS.Open('socketsbay.com', '443', nlTcp, 10000, true);
+  msg := ClientWS.WebSocketsUpgrade('wss/v2/2/demo/', '', false, [], lProto, '');
+  WSSResponses.Add('msg: ' + msg);
+end;
+
+
+begin
+  try
+    endConnection := false;
+    WSSResponses := TStringList.Create;
+    DoWSS;
+  finally
+    WSSResponses.Free;
+  end;
+end.
